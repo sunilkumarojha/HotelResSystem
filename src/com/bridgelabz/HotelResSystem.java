33f77920f0ebbf1b07ff7faa80e0f2d1b3007f31
sunilkumarojha <100593013+sunilkumarojha@users.noreply.github.com>
2022-05-07 07:14:24 +0530

    Cheapest Hotel

--- src/com/bridgelabz/HotelResSystem.java
+++ src/com/bridgelabz/HotelResSystem.java
@@ -1,4 +1,11 @@
-package com.bridgelabz;
+import java.util.ArrayList;
+import java.util.List;
 
-public class HotelResSystem {
+public class HotelReservationSystem {
+    List<Hotel> hotels = new ArrayList<>();
+
+    public boolean addHotel(Hotel hotel) {
+
+        return hotels.add(hotel);
+    }
 }
